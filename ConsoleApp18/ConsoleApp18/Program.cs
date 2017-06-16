2f0c21ac3917ed21bc127f830649f4c7fb0bee16
VenkataManthena <sandeep.varma590@gmail.com>
2017-06-16 00:51:26 -0500

    Add files via upload
    Console18 - Logging Exception in Animal Hospital

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+
+class MyClass
+{
+
+    static void Main(string[] args)
+    {
+
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        int[] numbers = new int[count];
+
+        string ip = Console.ReadLine();
+
+        string[] givenValues = ip.Split(' ');
+
+        for (int i = 0; i < givenValues.Length; i++)
+        {
+            numbers[i] = Convert.ToInt32(givenValues[i]);
+        }
+
+        int countMin = numbers[0];
+
+        for (int i = 0; i < givenValues.Length; i++)
+        {
+            if (numbers[i] < countMin)
+            {
+                countMin = numbers[i];
+            }
+        }
+
+        Console.WriteLine(countMin);
+
+
+
+        SqlConnection conn = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI");
+
+        SqlCommand cmd = new SqlCommand( "select * from Customers", conn);
+
+        SqlDataReader rdr = cmd.ExecuteReader();
+
+
+    }
+}
